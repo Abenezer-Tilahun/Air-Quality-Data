40027eb038ae29db0b76e4ba6397fc9040f35b37
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-04-18 17:45:30 +0300

    Fix the linter error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage/HomePage';
-import DetailsPage from './components/DetailsPage/DetailsPage';
+import HomePage from './components/Homepage/Homepage';
+import DetailsPage from './components/Detailspage/Detailspage';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => (
